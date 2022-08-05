179ad9fd442205d5350aabec3e4847355acd3a0b
JATIN YADAV <jatinyadav0000@gmail.com>
2022-08-05 09:51:15 +0530

    add customer page

--- base/urls.py
+++ base/urls.py
@@ -4,6 +4,6 @@ from base import views
 urlpatterns = [
     path('',views.home, name="home"),
     path('products', views.products, name="products"),
-    path('customer', views.customer, name="customer"),
+    path('customer/<str:pk>', views.customer, name="customer"),
 
 ]
--- base/views.py
+++ base/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from base.models import Customer, Order, Product
 # Create your views here.
 
 def home(request):
-    return render(request, 'base/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    panding = orders.filter(status='panding').count()
+    context = {'orders': orders,
+    'customers':customers, 'total_customer':total_customers, 'total_orders':total_orders, 'delivered': delivered, 'panding':panding}
+    return render(request, 'base/dashboard.html', context)
 
 def products(request):
-    return render(request, 'base/products.html')
+    products = Product.objects.all()
+    return render(request, 'base/products.html', {'products': products})
 
-def customer(request):
-    return render(request, 'base/customers.html')
+def customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all()
+    order_count = orders.count()
+    context= {'customer':customer, 'orders':orders, 'order_count': order_count}
+    return render(request, 'base/customers.html', context)
